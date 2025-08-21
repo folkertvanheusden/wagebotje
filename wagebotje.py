265c816b2f644b40da65ceea377f9fdc49ddc744
NURDSpace <mail@vanheusden.com>
2025-08-21 21:58:15 +0200

    poc

--- wagebotje.py
+++ wagebotje.py
@@ -9,12 +9,14 @@ import time
 
 from configuration import *
 
-def learn(text):
-    db_file = 'wagebotje.db'
+db_file = 'wagebotje.db'
+
+def learn(parts):
     con = sqlite3.connect(db_file)
 
     cur = con.cursor()
     try:
+        cur.execute('PRAGMA journal_mode=wal')
         cur.execute('CREATE TABLE start(start text, count integer, primary key (start))')
         cur.execute('CREATE TABLE word1(word1 text, count integer, primary key (word1))')
         cur.execute('CREATE TABLE word2(word1 text, word2 text, count integer, primary key (word1, word2))')
@@ -23,11 +25,11 @@ def learn(text):
     except:
         pass
     cur.close()
+    con.commit()
 
     cur = con.cursor()
     first = True
     pwords = []
-    parts = text.split()
     for word in parts:
         if first:
             first = False
@@ -55,6 +57,90 @@ def learn(text):
     con.commit()
     con.close()
 
+def generate_one_sentence(words):
+    con = sqlite3.connect(db_file)
+    try:
+        # start of sentence
+        cur = con.cursor()
+        cur.execute('select start, -LOG(RANDOM() + 1) / count AS priority, count FROM start WHERE start=? ORDER BY priority LIMIT 1', (words[0],))
+        start_word = cur.fetchone()
+        if start_word == None:
+            cur.execute('select start, -LOG(RANDOM() + 1) / count AS priority, count FROM start ORDER BY priority LIMIT 1')
+        start_word = cur.fetchone()
+        cur.close()
+        # end of sentence
+        cur = con.cursor()
+        cur.execute('select end, -LOG(RANDOM() + 1) / count AS priority, count FROM end ORDER BY priority LIMIT 1')
+        end_word = cur.fetchone()
+        cur.close()
+
+        target_length = random.randint(2, 10)
+        max_length = random.randint(target_length, 25)
+        output = [ (start_word[0], start_word[2]) ]
+        while len(output) < max_length:
+            cur = con.cursor()
+
+            row = None
+            if len(output) >= 3:
+                cur.execute('select word3, -LOG(RANDOM() + 1) / count AS priority, count FROM word3 WHERE word1=? AND word2=? ORDER BY priority LIMIT 1', (output[-2][0], output[-1][0]))
+                row = cur.fetchone()
+
+            if row == None or len(output) >= 2:
+                cur.execute('select word2, -LOG(RANDOM() + 1) / count AS priority, count FROM word2 WHERE word1=? ORDER BY priority LIMIT 1', (output[-1][0], ))
+                row = cur.fetchone()
+
+            if row == None:
+                cur.execute('select word1, -LOG(RANDOM() + 1) / count AS priority, count FROM word1 ORDER BY priority LIMIT 1')
+                row = cur.fetchone()
+
+            cur.close()
+
+            if row == None:
+                continue
+
+            output.append((row[0], row[2]))
+
+            if len(output) >= target_length:
+                cur = con.cursor()
+                cur.execute('SELECT COUNT(*) AS n FROM end WHERE end=? LIMIT 1', (output[-1][0],))
+                n_end = cur.fetchone()[0]
+                if n_end > 0:
+                    break
+
+        reply = ' '.join([token[0] for token in output])
+        reply_sum = sum([token[1] for token in output])
+
+        con.close()
+        return (reply, reply_sum)
+
+    except Exception as e:
+        con.close()
+        print(f'Exception: {e}, line number: {e.__traceback__.tb_lineno}')
+        raise e
+
+def generate_reply(tokens):
+    con = sqlite3.connect(db_file)
+
+    lowest = (None, 100000000000000000000)
+
+    choices = []
+    pwords = []
+    for word in tokens:
+        pwords.append(word)
+        while len(pwords) > 3:
+            del pwords[0]
+
+        # generate a sentence with each word, sum the counts for each word in such a sentence, do a weighted random selection of those sentences
+        rc = generate_one_sentence(pwords)
+        if not rc[0] is None:
+            choices.append(rc)
+            if rc[1] < lowest[1]:
+                lowest = rc
+
+    con.close()
+
+    return lowest[0]
+
 def on_message(client, userdata, message):
     global prefix
 
@@ -73,7 +159,8 @@ def on_message(client, userdata, message):
     if len(text) == 0:
         return
 
-    learn(text)
+    tokens = [ token.lower() for token in text.split(' ') ]
+    learn(tokens)
 
     parts   = topic.split('/')
     channel = parts[2] if len(parts) >= 3 else 'nurdbottest'  # default channel if can't be deduced
@@ -86,12 +173,13 @@ def on_message(client, userdata, message):
     if channel in channels or (len(channel) >= 1 and channel[0] == '\\'):
         response_topic = f'{topic_prefix}to/irc/{channel}/notice'
 
-        tokens  = text.split(' ')
-        command = tokens[0][1:]
+        command = tokens[0]
 
         if command == 'wagebotje:':
             try:
-                pass
+                reply = generate_reply(tokens)
+                if not reply is None:
+                    client.publish(response_topic, reply)
 
             except Exception as e:
                 client.publish(response_topic, f'Exception: {e}, line number: {e.__traceback__.tb_lineno}')
